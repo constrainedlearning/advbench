cdf3c58b22654515b81ec4914ce1e6fc3e1d0238
ihounie <ihounie@gmail.com>
2022-04-17 21:37:12 -0400

    fixed import after networks refactor

--- advbench/networks.py
+++ advbench/networks.py
@@ -1,6 +1,7 @@
 from advbench.models.e2_networks import e2wrn
 from advbench.models.e2_mnist import E2SFCNN, E2SFCNN_QUOT
 from advbench.models.wrn import wrn16_8, wrn28_10
+from advbench.models.resnet import ResNet18
 from advbench.models.mnist import MNISTNet, CnSteerableCNN, SteerableMNISTnet
 import torch.nn.init as init
 import numpy as np
@@ -45,7 +46,7 @@ def Classifier(input_shape, num_classes, hparams):
             return wrn28_10(num_classes=num_classes)
         elif hparams["model"] == "wrn-16-8":
             print("Using WRN-16-8")
-            return wrn16_8_stl(num_classes=num_classes)
+            return wrn16_8(num_classes=num_classes)
         elif hparams["model"] == "wrn-16-8-rot":
             print("Using e2 invariant WRN-16-8-rot")
             return e2wrn(depth=16, widen_factor = 8, num_classes=num_classes, r=3, fixparams=True)
