655d7928c07a4dbb974f289ff81bb2e80fbc2a10
ihounie <ihounie@gmail.com>
2022-04-18 08:52:32 -0400

    stl10 script

--- paper_experiments/stl.sh
+++ paper_experiments/stl.sh
@@ -2,13 +2,13 @@ for seed in 0
 do
     for perturbations in 'SE' 'Translation'
     do
-        for algo in ERM Augmentation Laplacian_DALE_PD_Reverse MH_DALE_PD_Reverse Adversarial_Adam Adversarial_Worst_of_K Adversarial_PGD Adam_DALE_PD_Reverse
+        for algo in ERM Augmentation MH_DALE_PD_Reverse Laplacian_DALE_PD_Reverse Adversarial_Adam Adversarial_Worst_Of_K Adversarial_PGD PGD_DALE_PD_Reverse Adam_DALE_PD_Reverse
         do
-            python -m advbench.scripts.train_no_validation --dataset MNIST --algorithm $algo --output_dir train-output --test_attacks Fo_PGD Fo_Adam Rand_Aug_Batch Worst_Of_K Gaussian_Batch Laplacian_Batch --perturbation SE --model wrn16_8 --seed $seed --device cuda:1 --aug
+            python -m advbench.scripts.train_no_validation --dataset STL10 --algorithm $algo --output_dir train-output --test_attacks Fo_PGD Fo_Adam Rand_Aug_Batch Gaussian_Batch Laplacian_Batch --perturbation SE --model wrn-16-8 --seed $seed --augment
         done
-        for algo in ERM Augmentation Laplacian_DALE_PD_Reverse MH_DALE_PD_Reverse Adversarial_Adam Adversarial_Worst_of_K Adversarial_PGD Adam_DALE_PD_Reverse
+        for algo in ERM Augmentation Laplacian_DALE_PD_Reverse MH_DALE_PD_Reverse Adversarial_Adam Adversarial_Worst_Of_K Adversarial_PGD Adam_DALE_PD_Reverse
         do
-            python -m advbench.scripts.train_no_validation --dataset MNIST --algorithm $algo --output_dir train-output --test_attacks Fo_PGD Fo_Adam Rand_Aug_Batch Worst_Of_K Gaussian_Batch Laplacian_Batch --perturbation SE --model wrn16_8 --seed $seed --device cuda:1
+            python -m advbench.scripts.train_no_validation --dataset STL10 --algorithm $algo --output_dir train-output --test_attacks Fo_PGD Fo_Adam Rand_Aug_Batch Gaussian_Batch Laplacian_Batch --perturbation SE --model wrn-16-8 --seed $seed
         done
     done
 done
