79b5fd61a41c17f0d7268f657680968214cca5d7
ihounie <ihounie@seas.upenn.edu>
2022-05-24 15:54:35 -0400

    little stuff

--- advbench/networks.py
+++ advbench/networks.py
@@ -69,7 +69,7 @@ def create_model(input_shape, num_classes, hparams):
             return wrn16_8(num_classes=num_classes)
         elif hparams["model"] == "wrn-16-8-rot":
             print("Using e2 invariant WRN-16-8-rot")
-            return e2wrn(depth=16, widen_factor = 8, num_classes=num_classes, r=3, fixparams=True)
+            return e2wrn(depth=16, widen_factor = 8, num_classes=num_classes, r=3, fixparams=False)
         elif hparams["model"] == "convnext-T":
             return timm.create_model('convnext_tiny', pretrained=True)
         elif hparams["model"] == "CnSteerableCNN":
