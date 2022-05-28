5b806ce614ecd75c2c8b4ab31031a27ea5bc01f1
ihounie <ihounie@gmail.com>
2022-05-28 12:52:35 -0400

    script

--- paper_experiments/cifar.sh
+++ paper_experiments/cifar.sh
@@ -1,9 +1,7 @@
-pushd advbench
-for seed in 0
+for seed in 0 1 2 3
 do
-for algo in ERM Augmentation Laplacian_DALE_PD_Reverse Adversarial_Worst_of_K Adversarial_Adam PGD MCMC_DALE_PD_Reverse
+for algo in Adversarial_Worst_Of_K
     do
-        python -m advbench.scripts.train_no_validation --dataset CIFAR --algorithm $algo --output_dir train-output --test_attacks Gaussian_Batch Laplacian_Batch LMC_Laplacian_Linf MCMC Grid_Batch PGD_Linf Rand_Aug_Batch Worst_Of_K --perturbation SE --model wrn-28-10 --log_imgs --seed $seed
+        python -m advbench.scripts.train_no_validation --dataset CIFAR100 --algorithm $algo --output_dir train-output --test_attacks Rand_Aug_Batch Fo_PGD Fo_Adam Gaussian_Batch Laplacian_Batch --perturbation SE --model wrn-28-10 --seed $seed
     done
 done
-popd
