ad94b7802105f8ec54872cb8fe56849ccce1c555
ihounie <ihounie@seas.upenn.edu>
2022-08-22 19:26:06 -0400

    pointcloud experiments

--- paper_experiments/pointclouds_pareto.sh
+++ paper_experiments/pointclouds_pareto.sh
@@ -1,12 +1,12 @@
-for seed in 0
+for seed in 0 1 2 3 4
 do
     for perturbations in 'PointcloudJitter'
     do
         for algo in 'Laplacian_DALE_PD_Reverse'
         do
-            for eps in 0.7 2.8
+            for eps in 0.7 0.9 1.4 1.95 2.8
             do
-                python -m advbench.scripts.train --dataset modelnet40 --algorithm $algo --output_dir train-output --test_attacks Fo_PGD Fo_Adam Rand_Aug_Batch Gaussian_Batch Laplacian_Batch --perturbation $perturbations --model DGCNN --seed $seed --eps $eps --flags pareto
+                python -m advbench.scripts.train --dataset modelnet40 --algorithm $algo --output_dir train-output --test_attacks Beta_aug Rand_Aug_Batch Fo_SGD --beta 0.25 0.25 0.25 0.25 0.5 0.5 0.5 0.5 1.0 1.0 1.0 1.0 2.0 2.0 2.0 2.0 --alpha 0.25 0.5 1.0 2.0 0.25 0.5 1.0 2.0 0.25 0.5 1.0 2.0 0.25 0.5 1.0 2.0 --perturbation $perturbations --model DGCNN --seed $seed --eps $eps --flags final
             done
         done
     done
